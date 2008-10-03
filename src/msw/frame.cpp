romeugodoi/wxWidgets

commit 920d278b1b678f3e75c62b8103b79f20999cfa95

avoid duplicated command events if a wxFrame-level handler called event.Skip() (#4757)
git-svn-id: http://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@54510 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/frame.cpp
@@ -1053,7 +1053,12 @@ WXLRESULT wxFrame::MSWWindowProc(WXUINT message, WXWPARAM wParam, WXLPARAM lPara
                 UnpackCommand((WXWPARAM)wParam, (WXLPARAM)lParam,
                               &id, &hwnd, &cmd);
 
-                processed = HandleCommand(id, cmd, (WXHWND)hwnd);
+                HandleCommand(id, cmd, (WXHWND)hwnd);
+
+                // don't pass WM_COMMAND to the base class as it would generate
+                // another wxCommandEvent which would result in its handler
+                // being called twice if it uses event.Skip()
+                processed = true;
             }
             break;
 

commit 037bda57258b86cd6a8c8f80b46a50e158b79ae8

generate command events for accelerators even when there is no menu bar (#9784) fixing the bug introduced in r54510
git-svn-id: http://svn.wxwidgets.org/svn/wx/wxWidgets/branches/WX_2_8_BRANCH@56069 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/frame.cpp
@@ -954,34 +954,20 @@ bool wxFrame::HandleCommand(WXWORD id, WXWORD cmd, WXHWND control)
             return win->MSWCommand(cmd, id);
     }
 
-    // handle here commands from menus and accelerators
-    if ( cmd == 0 || cmd == 1 )
+    // handle here commands from menus and accelerators for our menu bar items,
+    // all the rest is handled by wxWindow itself
+    if ( !control && (cmd == 0 /* menu */ || cmd == 1 /* accel */) )
     {
 #if wxUSE_MENUS_NATIVE
-        if ( wxCurrentPopupMenu )
-        {
-            wxMenu *popupMenu = wxCurrentPopupMenu;
-            wxCurrentPopupMenu = NULL;
-
-            return popupMenu->MSWCommand(cmd, id);
-        }
+        if ( !wxCurrentPopupMenu )
 #endif // wxUSE_MENUS_NATIVE
-
-#if defined(__SMARTPHONE__) && defined(__WXWINCE__)
-        // handle here commands from Smartphone menu bar
-        if ( wxTopLevelWindow::HandleCommand(id, cmd, control ) )
         {
-            return true;
-        }
-#endif // __SMARTPHONE__ && __WXWINCE__
-
-        if ( ProcessCommand(id) )
-        {
-            return true;
+            if ( GetMenuBar() && ProcessCommand(id) )
+                return true;
         }
     }
 
-    return false;
+    return wxFrameBase::HandleCommand(id, cmd, control);
 }
 
 bool wxFrame::HandleMenuSelect(WXWORD nItem, WXWORD flags, WXHMENU hMenu)