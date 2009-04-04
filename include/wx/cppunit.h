romeugodoi/wxWidgets

commit 03c4a31c06ca35a8fef6f91f8041ec6bed4af1f0

allow symmetric comparisons to work too in CPPUNIT_ASSERT_EQUAL (e.g. wxString==char*, size_t==int and not only char*==wxString, int==size_t)
git-svn-id: http://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@60017 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

include/wx/cppunit.h
@@ -97,12 +97,30 @@ assertEquals(const char *expected,
 }
 
 inline void
+assertEquals(const wxString& expected,
+             const char *actual,
+             CppUnit::SourceLine sourceLine,
+             const std::string& message)
+{
+    assertEquals(expected, wxString(actual), sourceLine, message);
+}
+
+inline void
 assertEquals(const wchar_t *expected,
              const wxString& actual,
              CppUnit::SourceLine sourceLine,
              const std::string& message)
 {
     assertEquals(wxString(expected), actual, sourceLine, message);
+}
+
+inline void
+assertEquals(const wxString& expected,
+             const wchar_t *actual,
+             CppUnit::SourceLine sourceLine,
+             const std::string& message)
+{
+    assertEquals(expected, wxString(actual), sourceLine, message);
 }
 
 CPPUNIT_NS_END
@@ -137,6 +155,7 @@ CPPUNIT_NS_END
     #define WX_CPPUNIT_ALLOW_EQUALS_TO_INT(T) \
         CPPUNIT_NS_BEGIN \
             WX_CPPUNIT_ASSERT_EQUALS(int, T) \
+            WX_CPPUNIT_ASSERT_EQUALS(T, int) \
             WX_CPPUNIT_ASSERT_EQUALS(T, T) \
         CPPUNIT_NS_END
 
@@ -147,6 +166,7 @@ CPPUNIT_NS_END
     #define WX_CPPUNIT_ALLOW_EQUALS_TO_INT(T) \
         CPPUNIT_NS_BEGIN \
             WX_CPPUNIT_ASSERT_EQUALS(int, T) \
+            WX_CPPUNIT_ASSERT_EQUALS(T, int) \
         CPPUNIT_NS_END
 #endif // VC6/!VC6
 

commit 14973886e4d430938a266bc72fe606fe213d16c0

revert r60023 and really fixed compilation problems caused by r60017 by providing assertEquals(const char *, const char *) overload as well
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@60024 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

include/wx/cppunit.h
@@ -89,6 +89,15 @@ CPPUNIT_NS_BEGIN
 // compare wxStrings directly with C strings
 inline void
 assertEquals(const char *expected,
+             const char *actual,
+             CppUnit::SourceLine sourceLine,
+             const std::string& message)
+{
+    assertEquals(wxString(expected), wxString(actual), sourceLine, message);
+}
+
+inline void
+assertEquals(const char *expected,
              const wxString& actual,
              CppUnit::SourceLine sourceLine,
              const std::string& message)

tests/strings/crt.cpp
@@ -207,8 +207,8 @@ void CrtTestCase::Strpbrk()
     CPPUNIT_ASSERT_EQUAL(  ',', *wxStrpbrk(strWX.c_str(), s.mb_str()) );
     CPPUNIT_ASSERT_EQUAL( L',', *wxStrpbrk(strWX.c_str(), s.wc_str()) );
 
-    CPPUNIT_ASSERT_EQUAL( (char *)NULL, wxStrpbrk(strWX, "xyz") );
-    CPPUNIT_ASSERT_EQUAL( (wchar_t *)NULL, wxStrpbrk(strWX.c_str(), L"xyz") );
+    CPPUNIT_ASSERT( !wxStrpbrk(strWX, "xyz") );
+    CPPUNIT_ASSERT( !wxStrpbrk(strWX.c_str(), L"xyz") );
 }
 
 void CrtTestCase::Strnlen()

tests/strings/strings.cpp
@@ -943,9 +943,9 @@ void StringTestCase::ScopedBuffers()
     // but assigning it to wxCharBuffer makes a full copy
     wxCharBuffer buf(sbuf);
     CPPUNIT_ASSERT( buf.data() != literal );
-    CPPUNIT_ASSERT_EQUAL( literal, buf );
+    CPPUNIT_ASSERT_EQUAL( literal, buf.data() );
 
     wxCharBuffer buf2 = sbuf;
     CPPUNIT_ASSERT( buf2.data() != literal );
-    CPPUNIT_ASSERT_EQUAL( literal, buf );
+    CPPUNIT_ASSERT_EQUAL( literal, buf.data() );
 }