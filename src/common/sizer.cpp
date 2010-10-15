romeugodoi/wxWidgets

commit 09cdbea1716a60b9c567c3f2e525056310e8ed3d

added support for custom labels in wxMessageBox for wxGTK too
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@55482 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/gtk/msgdlg.cpp
@@ -25,6 +25,7 @@
 #endif
 
 #include "wx/gtk/private.h"
+#include "wx/gtk/private/mnemonics.h"
 #include <gtk/gtk.h>
 
 #if wxUSE_LIBHILDON
@@ -38,11 +39,36 @@ wxMessageDialog::wxMessageDialog(wxWindow *parent,
                                  const wxString& caption,
                                  long style,
                                  const wxPoint& WXUNUSED(pos))
-               : wxMessageDialogBase(GetParentForModalDialog(parent),
-                                     message,
-                                     caption,
-                                     style)
+               : wxMessageDialogWithCustomLabels(GetParentForModalDialog(parent),
+                                                 message,
+                                                 caption,
+                                                 style)
 {
+}
+
+wxString wxMessageDialog::GetDefaultYesLabel() const
+{
+    return GTK_STOCK_YES;
+}
+
+wxString wxMessageDialog::GetDefaultNoLabel() const
+{
+    return GTK_STOCK_NO;
+}
+
+wxString wxMessageDialog::GetDefaultOKLabel() const
+{
+    return GTK_STOCK_OK;
+}
+
+wxString wxMessageDialog::GetDefaultCancelLabel() const
+{
+    return GTK_STOCK_CANCEL;
+}
+
+void wxMessageDialog::DoSetCustomLabel(wxString& var, const wxString& value)
+{
+    var = wxConvertMnemonicsToGTK(value);
 }
 
 void wxMessageDialog::GTKCreateMsgDialog()
@@ -74,22 +100,22 @@ void wxMessageDialog::GTKCreateMsgDialog()
                );
 #else // !wxUSE_LIBHILDON
     GtkMessageType type = GTK_MESSAGE_ERROR;
-    GtkButtonsType buttons = GTK_BUTTONS_OK;
+    GtkButtonsType buttons = GTK_BUTTONS_NONE;
 
-    if (m_dialogStyle & wxYES_NO)
+    // when using custom labels, we have to add all the buttons ourselves
+    if ( !HasCustomLabels() )
     {
-        if (m_dialogStyle & wxCANCEL)
-            buttons = GTK_BUTTONS_NONE;
-        else
-            buttons = GTK_BUTTONS_YES_NO;
-    }
-
-    if (m_dialogStyle & wxOK)
-    {
-        if (m_dialogStyle & wxCANCEL)
-            buttons = GTK_BUTTONS_OK_CANCEL;
-        else
-            buttons = GTK_BUTTONS_OK;
+        if ( m_dialogStyle & wxYES_NO )
+        {
+            if ( !(m_dialogStyle & wxCANCEL) )
+                buttons = GTK_BUTTONS_YES_NO;
+            //else: no standard GTK_BUTTONS_YES_NO_CANCEL so leave as NONE
+        }
+        else if ( m_dialogStyle & wxOK )
+        {
+            buttons = m_dialogStyle & wxCANCEL ? GTK_BUTTONS_OK_CANCEL
+                                               : GTK_BUTTONS_OK;
+        }
     }
 
     if (m_dialogStyle & wxICON_EXCLAMATION)
@@ -145,39 +171,43 @@ void wxMessageDialog::GTKCreateMsgDialog()
     if (m_caption != wxMessageBoxCaptionStr)
         gtk_window_set_title(GTK_WINDOW(m_widget), wxGTK_CONV(m_caption));
 
-    // we need to add dialogs manually when using Yes/No/Cancel dialog as GTK+
-    // doesn't support it natively and when using Hildon we add all the buttons
-    // manually as it doesn't support too many of the combinations we have
     GtkDialog * const dlg = GTK_DIALOG(m_widget);
-    if ( m_dialogStyle & wxYES_NO )
-    {
-        if ( m_dialogStyle & wxCANCEL )
-        {
-            gtk_dialog_add_button(dlg, GTK_STOCK_NO, GTK_RESPONSE_NO);
-            gtk_dialog_add_button(dlg, GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
-            gtk_dialog_add_button(dlg, GTK_STOCK_YES, GTK_RESPONSE_YES);
-        }
+
+    // we need to add buttons manually if we use custom labels or always for
+    // Yes/No/Cancel dialog as GTK+ doesn't support it natively and when using
+    // Hildon we add all the buttons manually as it doesn't support too many of
+    // the combinations we may have
 #if wxUSE_LIBHILDON
-        else // just Yes/No
+    static const bool addButtons = true;
+#else // !wxUSE_LIBHILDON
+    const bool addButtons = buttons == GTK_BUTTONS_NONE;
+#endif // wxUSE_LIBHILDON/!wxUSE_LIBHILDON
+
+    if ( m_dialogStyle & wxYES_NO ) // Yes/No or Yes/No/Cancel dialog
+    {
+        if ( addButtons )
         {
-            gtk_dialog_add_button(dlg, GTK_STOCK_NO, GTK_RESPONSE_NO);
-            gtk_dialog_add_button(dlg, GTK_STOCK_YES, GTK_RESPONSE_YES);
+            gtk_dialog_add_button(dlg, GetNoLabel(), GTK_RESPONSE_NO);
+            gtk_dialog_add_button(dlg, GetYesLabel(), GTK_RESPONSE_YES);
+
+            if ( m_dialogStyle & wxCANCEL )
+            {
+                gtk_dialog_add_button(dlg, GetCancelLabel(),
+                                      GTK_RESPONSE_CANCEL);
+            }
         }
-#endif // wxUSE_LIBHILDON
 
         gtk_dialog_set_default_response(dlg,
                                         m_dialogStyle & wxNO_DEFAULT
                                             ? GTK_RESPONSE_NO
                                             : GTK_RESPONSE_YES);
     }
-#if wxUSE_LIBHILDON
-    else // Ok or Ok/Cancel dialog
+    else if ( addButtons ) // Ok or Ok/Cancel dialog
     {
-        gtk_dialog_add_button(dlg, GTK_STOCK_OK, GTK_RESPONSE_OK);
+        gtk_dialog_add_button(dlg, GetOKLabel(), GTK_RESPONSE_OK);
         if ( m_dialogStyle & wxCANCEL )
-            gtk_dialog_add_button(dlg, GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL);
+            gtk_dialog_add_button(dlg, GetCancelLabel(), GTK_RESPONSE_CANCEL);
     }
-#endif // wxUSE_LIBHILDON
 }
 
 int wxMessageDialog::ShowModal()

include/wx/gtk/msgdlg.h
@@ -12,7 +12,7 @@
 #ifndef _WX_GTK_MSGDLG_H_
 #define _WX_GTK_MSGDLG_H_
 
-class WXDLLIMPEXP_CORE wxMessageDialog : public wxMessageDialogBase
+class WXDLLIMPEXP_CORE wxMessageDialog : public wxMessageDialogWithCustomLabels
 {
 public:
     wxMessageDialog(wxWindow *parent, const wxString& message,
@@ -21,7 +21,7 @@ public:
                     const wxPoint& pos = wxDefaultPosition);
 
     virtual int ShowModal();
-    virtual bool Show( bool WXUNUSED(show) = true ) { return false; };
+    virtual bool Show(bool WXUNUSED(show) = true) { return false; }
 
 protected:
     // implement some base class methods to do nothing to avoid asserts and
@@ -33,6 +33,15 @@ protected:
                               int WXUNUSED(width), int WXUNUSED(height)) {}
 
 private:
+    // override to use stock GTK+ defaults instead of just string ones
+    virtual wxString GetDefaultYesLabel() const;
+    virtual wxString GetDefaultNoLabel() const;
+    virtual wxString GetDefaultOKLabel() const;
+    virtual wxString GetDefaultCancelLabel() const;
+
+    // override to convert wx mnemonics to GTK+ ones
+    virtual void DoSetCustomLabel(wxString& var, const wxString& value);
+
     // create the real GTK+ dialog: this is done from ShowModal() to allow
     // changing the message between constructing the dialog and showing it
     void GTKCreateMsgDialog();

include/wx/msgdlg.h
@@ -117,7 +117,10 @@ protected:
 // functions for setting these labels immediately and we also don't need to
 // store them at all if custom labels are not supported, which is why we do
 // this in a separate class and not wxMessageDialogBase itself)
-#if defined(__WXCOCOA__) || defined(__WXMAC__) || defined(__WXMSW__)
+#if defined(__WXCOCOA__) || \
+    defined(__WXGTK20__) || \
+    defined(__WXMAC__) || \
+    defined(__WXMSW__)
 
 class WXDLLIMPEXP_CORE wxMessageDialogWithCustomLabels
     : public wxMessageDialogBase
@@ -175,11 +178,14 @@ protected:
     // these functions return the label to be used for the button which is
     // either a custom label explicitly set by the user or the default label,
     // i.e. they always return a valid string
-    wxString GetYesLabel() const { return m_yes.empty() ? _("Yes") : m_yes; }
-    wxString GetNoLabel() const { return m_no.empty() ? _("No") : m_no; }
-    wxString GetOKLabel() const { return m_ok.empty() ? _("OK") : m_ok; }
+    wxString GetYesLabel() const
+        { return m_yes.empty() ? GetDefaultYesLabel() : m_yes; }
+    wxString GetNoLabel() const
+        { return m_no.empty() ? GetDefaultNoLabel() : m_no; }
+    wxString GetOKLabel() const
+        { return m_ok.empty() ? GetDefaultOKLabel() : m_ok; }
     wxString GetCancelLabel() const
-        { return m_cancel.empty() ? _("Cancel") : m_cancel; }
+        { return m_cancel.empty() ? GetDefaultCancelLabel() : m_cancel; }
 
 private:
     // this function is called by our public SetXXXLabels() and should assign
@@ -189,6 +195,13 @@ private:
     {
         var = value;
     }
+
+    // these functions may be overridden to provide different defaults for the
+    // default button labels (this is used by wxGTK)
+    virtual wxString GetDefaultYesLabel() const { return _("Yes"); }
+    virtual wxString GetDefaultNoLabel() const { return _("No"); }
+    virtual wxString GetDefaultOKLabel() const { return _("OK"); }
+    virtual wxString GetDefaultCancelLabel() const { return _("Cancel"); }
 
     // labels for the buttons, initially empty meaning that the defaults should
     // be used, use GetYes/No/OK/CancelLabel() to access them

commit 073402a266e2a72a72916fad267b45807db1fbe8

Fix button order in wxGTK wxMessageDialog and wxStdDialogButtonSizer.
The order of buttons in wxGTK wxMessageDialog was accidentally broken since
r55482 and wxStdDialogButtonSizer implementation for wxGTK was subsequently
modified to work in the same way as the broken version of wxMessageDialog.

Fix wxMessageDialog and remove the "compatibility" workarounds from
wxStdDialogButtonSizer to restore correct behaviour in both places.

Closes #12565.

git-svn-id: http://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@65812 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/common/sizer.cpp
@@ -2526,29 +2526,6 @@ void wxStdDialogButtonSizer::SetCancelButton( wxButton *button )
     m_buttonCancel = button;
 }
 
-#ifdef __WXGTK20__
-
-namespace
-{
-
-// Returns true only if the button is non-NULL and has the given id and text
-// (possible translated).
-bool IsStdButtonWithStdText(wxButton *btn, wxWindowID id, const char *label)
-{
-    if ( !btn )
-        return false;
-
-    if ( btn->GetId() != id )
-        return false;
-
-    const wxString labelText = btn->GetLabelText();
-    return labelText == label || labelText == wxGetTranslation(label);
-}
-
-} // anonymous namespace
-
-#endif // __WXGTK20__
-
 void wxStdDialogButtonSizer::Realize()
 {
 #ifdef __WXMAC__
@@ -2591,12 +2568,9 @@ void wxStdDialogButtonSizer::Realize()
         Add(12, 40);
 #elif defined(__WXGTK20__)
         // http://library.gnome.org/devel/hig-book/stable/windows-alert.html.en
-        // describes the margins and the buttons order but basically it is
+        // says that the correct button order is
         //
         //      [Help]                  [Alternative] [Cancel] [Affirmative]
-        //
-        // in general case but, somewhat confusingly, the native message box
-        // uses "No Yes Cancel" with these particular buttons so do we as well.
 
         // Flags ensuring that margins between the buttons are 6 pixels.
         const wxSizerFlags
@@ -2611,31 +2585,17 @@ void wxStdDialogButtonSizer::Realize()
         // Align the rest of the buttons to the right.
         AddStretchSpacer();
 
-        // "No Yes Cancel" is an exception to the general rule according to
-        // which the affirmative buttons goes after "Cancel" so treat it
-        // separately
-        if ( IsStdButtonWithStdText(m_buttonNegative, wxID_NO, "No") &&
-              IsStdButtonWithStdText(m_buttonAffirmative, wxID_YES, "Yes") &&
-               IsStdButtonWithStdText(m_buttonCancel, wxID_CANCEL, "Cancel") )
-        {
+        if (m_buttonNegative)
             Add(m_buttonNegative, flagsBtn);
-            Add(m_buttonAffirmative, flagsBtn);
+
+        if (m_buttonApply)
+            Add(m_buttonApply, flagsBtn);
+
+        if (m_buttonCancel)
             Add(m_buttonCancel, flagsBtn);
-        }
-        else // Use standard layout
-        {
-            if (m_buttonNegative)
-                Add(m_buttonNegative, flagsBtn);
 
-            if (m_buttonApply)
-                Add(m_buttonApply, flagsBtn);
-
-            if (m_buttonCancel)
-                Add(m_buttonCancel, flagsBtn);
-
-            if (m_buttonAffirmative)
-                Add(m_buttonAffirmative, flagsBtn);
-        }
+        if (m_buttonAffirmative)
+            Add(m_buttonAffirmative, flagsBtn);
 
         // Ensure that the right margin is 12 as well.
         AddSpacer(9);

src/gtk/msgdlg.cpp
@@ -215,16 +215,23 @@ void wxMessageDialog::GTKCreateMsgDialog()
     {
         if ( addButtons )
         {
+            // Add the buttons in the correct order which is, according to
+            // http://library.gnome.org/devel/hig-book/stable/windows-alert.html.en
+            // the following one:
+            //
+            // [Help]                  [Alternative] [Cancel] [Affirmative]
+
             gtk_dialog_add_button(dlg, wxGTK_CONV(GetNoLabel()),
                                   GTK_RESPONSE_NO);
-            gtk_dialog_add_button(dlg, wxGTK_CONV(GetYesLabel()),
-                                  GTK_RESPONSE_YES);
 
             if ( m_dialogStyle & wxCANCEL )
             {
                 gtk_dialog_add_button(dlg, wxGTK_CONV(GetCancelLabel()),
                                       GTK_RESPONSE_CANCEL);
             }
+
+            gtk_dialog_add_button(dlg, wxGTK_CONV(GetYesLabel()),
+                                  GTK_RESPONSE_YES);
         }
 
         // it'd probably be harmless to call gtk_dialog_set_default_response()