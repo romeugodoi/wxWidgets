romeugodoi/wxWidgets

commit 186fa909978d2fa258190cb163aae010c8fa80a0

* Implement dynamic loading of the Cairo DLL on Windows similar to how it was   done for GDI+.
* Enable the use of the wxCairoContext on MSW.

* Enable creating a wxGCDC from an exisiting wxGraphicsContext.

* Since it's possible for a DLL that is using wx to not be on the PATH nor in
  the same location as the .exe, change the wxDynamicLibrary::RawLoad method to
  explicitly look first in the same place as the main wx-using binary.  This way
  it will find DLLs that are in the same folder as the wx-using binary even if
  that would not be in the normal DLL search path.  

* Change wxDCImpl and wxDC::GetLogicalScale to be const methods.



git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@68935 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/dlmsw.cpp
@@ -27,6 +27,7 @@
 
 #include "wx/msw/private.h"
 #include "wx/msw/debughlp.h"
+#include "wx/filename.h"
 
 const wxString wxDynamicLibrary::ms_dllext(wxT(".dll"));
 
@@ -224,13 +225,37 @@ wxDllType wxDynamicLibrary::GetProgramHandle()
 // loading/unloading DLLs
 // ----------------------------------------------------------------------------
 
+#ifndef MAX_PATH
+    #define MAX_PATH 260        // from VC++ headers
+#endif
+
 /* static */
 wxDllType
 wxDynamicLibrary::RawLoad(const wxString& libname, int flags)
 {
-    return flags & wxDL_GET_LOADED
-            ? ::GetModuleHandle(libname.t_str())
-            : ::LoadLibrary(libname.t_str());
+    if (flags & wxDL_GET_LOADED)
+        return ::GetModuleHandle(libname.t_str());
+
+    // Explicitly look in the same path as where the main wx HINSTANCE module
+    // is located (usually the executable or the DLL that uses wx).  Normally
+    // this is automatically part of the default search path but in some cases
+    // it may not be, such as when the wxPython extension modules need to load
+    // a DLL, but the intperpreter executable is located elsewhere.  Doing
+    // this allows us to always be able to dynamically load a DLL that is
+    // located at the same place as the wx modules.
+    wxString modpath, path;
+    ::GetModuleFileName(wxGetInstance(),
+                        wxStringBuffer(modpath, MAX_PATH+1),
+                        MAX_PATH);
+    
+    wxFileName::SplitPath(modpath, &path, NULL, NULL);
+    ::SetDllDirectory(path.t_str());
+    
+    wxDllType handle = ::LoadLibrary(libname.t_str());
+
+    // reset the search path
+    ::SetDllDirectory(NULL);
+    return handle;
 }
 
 /* static */

build/tools/build-wxwidgets.py
@@ -174,6 +174,7 @@ def main(scriptName, args):
         "mac_framework" : (False, "Install the Mac build as a framework"),
         "mac_framework_prefix" 
                         : (defFwPrefix, "Prefix where the framework should be installed. Default: %s" % defFwPrefix),
+        "cairo"         : (False, "Enable dynamicly loading the Cairo lib for wxGraphicsContext on MSW"),
         "no_config"     : (False, "Turn off configure step on autoconf builds"),
         "config_only"   : (False, "Only run the configure step and then exit"),
         "rebake"        : (False, "Regenerate Bakefile and autoconf files"),
@@ -182,7 +183,6 @@ def main(scriptName, args):
         "cocoa"         : (False, "Build the old Mac Cooca port."),
         "osx_cocoa"     : (False, "Build the new Cocoa port"),
         "shared"        : (False, "Build wx as a dynamic library"),
-        "cairo"         : (False, "Build support for wxCairoContext (always true on GTK+)"),
         "extra_make"    : ("", "Extra args to pass on [n]make's command line."),
         "features"      : ("", "A comma-separated list of wxUSE_XYZ defines on Win, or a list of configure flags on unix."),
         "verbose"       : (False, "Print commands as they are run, (to aid with debugging this script)"),
@@ -343,6 +343,8 @@ def main(scriptName, args):
                 flags["wxUSE_UNICODE_MSLU"] = "1"
     
         if options.cairo:
+            if not os.environ.get("CAIRO_ROOT"):
+                print "WARNING: Expected CAIRO_ROOT set in the environment!"
             flags["wxUSE_CAIRO"] = "1"
     
         if options.wxpython:
@@ -361,7 +363,7 @@ def main(scriptName, args):
 
             if VERSION >= (2,9):
                 flags["wxUSE_UIACTIONSIMULATOR"] = "1"
-                
+
     
         mswIncludeDir = os.path.join(wxRootDir, "include", "wx", "msw")
         setup0File = os.path.join(mswIncludeDir, "setup0.h")
@@ -399,8 +401,11 @@ def main(scriptName, args):
                    
             if options.shared:
                 args.append("SHARED=1")
+
             if options.cairo:
-                args.append("USE_CAIRO=1")
+                args.append(
+                    "CPPFLAGS=/I%s" %
+                     os.path.join(os.environ.get("CAIRO_ROOT", ""), 'include\\cairo'))
     
             wxBuilder = builder.MSVCBuilder()
             

include/wx/cairo.h
@@ -16,130 +16,13 @@
 #include "wx/dynlib.h"
 #include <cairo.h>
 
-
-class wxCairoLibrary
+extern "C"
 {
-public:
-    // return the pointer to the global instance of this class or NULL if we
-    // failed to load/initialize it
-    static wxCairoLibrary *Get();
+    
+bool wxCairoInit();
+void wxCairoCleanUp();
 
-
-    // for internal use only
-    static void CleanUp();
-
-private:
-    // the single wxCairoLibrary instance or NULL
-    static wxCairoLibrary *ms_lib;
-
-    wxCairoLibrary();
-    ~wxCairoLibrary();
-
-    bool IsOk();
-    bool InitializeMethods();
-
-    wxDynamicLibrary m_libCairo;
-    wxDynamicLibrary m_libPangoCairo;
-
-    // true if we successfully loaded the libraries and can use them
-    //
-    // note that this field must have this name as it's used by wxDL_XXX macros
-    bool m_ok;
-
-public:
-    wxDL_VOIDMETHOD_DEFINE( cairo_arc,
-        (cairo_t *cr, double xc, double yc, double radius, double angle1, double angle2), (cr, xc, yc, radius, angle1, angle2) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_arc_negative,
-        (cairo_t *cr, double xc, double yc, double radius, double angle1, double angle2), (cr, xc, yc, radius, angle1, angle2) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_clip,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_close_path,
-        (cairo_t *cr), (cr) )
-    wxDL_METHOD_DEFINE( cairo_t*, cairo_create,
-        (cairo_surface_t *target), (target), NULL)
-    wxDL_VOIDMETHOD_DEFINE( cairo_curve_to,
-        (cairo_t *cr, double x1, double y1, double x2, double y2, double x3, double y3), (cr, x1, y1, x2, y2, x3, y3) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_destroy,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_fill,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_fill_preserve,
-        (cairo_t *cr), (cr) )
-    wxDL_METHOD_DEFINE( cairo_surface_t*, cairo_get_target,
-        (cairo_t *cr), (cr), NULL)
-    wxDL_METHOD_DEFINE( cairo_surface_t*, cairo_image_surface_create_for_data,
-        (unsigned char *data, cairo_format_t format, int width, int height, int stride), (data, format, width, height, stride), NULL)
-    wxDL_VOIDMETHOD_DEFINE( cairo_line_to,
-        (cairo_t *cr, double x, double y), (cr, x, y) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_move_to,
-        (cairo_t *cr, double x, double y), (cr, x, y) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_new_path,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_paint,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_pattern_add_color_stop_rgba,
-        (cairo_pattern_t *pattern, double offset, double red, double green, double blue, double alpha), (pattern, offset, red, green, blue, alpha) )
-    wxDL_METHOD_DEFINE( cairo_pattern_t*, cairo_pattern_create_for_surface,
-        (cairo_surface_t *surface), (surface), NULL)
-    wxDL_METHOD_DEFINE( cairo_pattern_t*, cairo_pattern_create_linear,
-        (double x0, double y0, double x1, double y1), (x0, y0, x1, y1), NULL)
-    wxDL_METHOD_DEFINE( cairo_pattern_t*, cairo_pattern_create_radial,
-        (double cx0, double cy0, double radius0, double cx1, double cy1, double radius1), (cx0, cy0, radius0, cx1, cy1, radius1), NULL)
-    wxDL_VOIDMETHOD_DEFINE( cairo_pattern_destroy,
-        (cairo_pattern_t *pattern), (pattern) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_pattern_set_extend,
-        (cairo_pattern_t *pattern, cairo_extend_t extend), (pattern, extend) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_pattern_set_filter,
-        (cairo_pattern_t *pattern, cairo_filter_t filter), (pattern, filter) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_rectangle,
-        (cairo_t *cr, double x, double y, double width, double height), (cr, x, y, width, height) )
-    wxDL_METHOD_DEFINE( cairo_t*, cairo_reference,
-        (cairo_t *cr), (cr), NULL )
-    wxDL_VOIDMETHOD_DEFINE( cairo_reset_clip,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_restore,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_rotate,
-        (cairo_t *cr, double angle), (cr, angle) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_save,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_scale,
-        (cairo_t *cr, double sx, double sy), (cr, sx, sy) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_dash,
-        (cairo_t *cr, const double *dashes, int num_dashes, double offset), (cr, dashes, num_dashes, offset) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_fill_rule,
-        (cairo_t *cr, cairo_fill_rule_t fill_rule), (cr, fill_rule) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_line_cap,
-        (cairo_t *cr, cairo_line_cap_t line_cap), (cr, line_cap) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_line_join,
-        (cairo_t *cr, cairo_line_join_t line_join), (cr, line_join) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_line_width,
-        (cairo_t *cr, double width), (cr, width) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_operator,
-        (cairo_t *cr, cairo_operator_t op), (cr, op) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_source,
-        (cairo_t *cr, cairo_pattern_t *source), (cr, source) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_set_source_rgba,
-        (cairo_t *cr, double red, double green, double blue, double alpha), (cr, red, green, blue, alpha) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_stroke,
-        (cairo_t *cr), (cr) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_stroke_preserve,
-        (cairo_t *cr), (cr) )
-    wxDL_METHOD_DEFINE( cairo_surface_t*, cairo_surface_create_similar,
-        (cairo_surface_t *other, cairo_content_t content, int width, int height), (other, content, width, height), NULL)
-    wxDL_VOIDMETHOD_DEFINE( cairo_surface_destroy,
-        (cairo_surface_t *surface), (surface) )
-    wxDL_VOIDMETHOD_DEFINE( cairo_translate,
-        (cairo_t *cr, double tx, double ty), (cr, tx, ty) )
-
-#if wxUSE_PANGO
-    wxDL_VOIDMETHOD_DEFINE( pango_cairo_update_layout,
-        (cairo_t *cr, PangoLayout *layout), (cr, layout) )
-    wxDL_VOIDMETHOD_DEFINE( pango_cairo_show_layout,
-        (cairo_t *cr, PangoLayout *layout), (cr, layout) )
-#endif
-    wxDECLARE_NO_COPY_CLASS(wxCairoLibrary);
-};
+}
 
 #endif // wxUSE_CAIRO
 

include/wx/dc.h
@@ -475,7 +475,7 @@ public:
     }
 
     virtual void SetLogicalScale(double x, double y);
-    virtual void GetLogicalScale(double *x, double *y)
+    virtual void GetLogicalScale(double *x, double *y) const
     {
         if ( x ) *x = m_logicalScaleX;
         if ( y ) *y = m_logicalScaleY;
@@ -1020,7 +1020,7 @@ public:
 
     void SetLogicalScale(double x, double y)
         { m_pimpl->SetLogicalScale( x, y ); }
-    void GetLogicalScale(double *x, double *y)
+    void GetLogicalScale(double *x, double *y) const
         { m_pimpl->GetLogicalScale( x, y ); }
 
     void SetLogicalOrigin(wxCoord x, wxCoord y)

include/wx/dcgraph.h
@@ -32,6 +32,8 @@ public:
 #if defined(__WXMSW__) && wxUSE_ENH_METAFILE
     wxGCDC( const wxEnhMetaFileDC& dc );
 #endif
+    wxGCDC(wxGraphicsContext* context);
+    
     wxGCDC();
     virtual ~wxGCDC();
 

commit 0526715ac26c837d777a0f04d4e5fbc59919e63d

Fixed dlmsw.cpp compilation with older SDKs and at run-time for systems prior to XP SP1.
Kernel32's SetDllDirectory is only available since XP SP1 and as such also not available in the SDK that comes by default with for example VS6. Attempt to retrieve the function from the DLL at run-time instead. Regression since r68935.

git-svn-id: http://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@68962 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/dlmsw.cpp
@@ -249,12 +249,37 @@ wxDynamicLibrary::RawLoad(const wxString& libname, int flags)
                         MAX_PATH);
     
     wxFileName::SplitPath(modpath, &path, NULL, NULL);
-    ::SetDllDirectory(path.t_str());
-    
+
+    typedef BOOL (WINAPI *SetDllDirectory_t)(LPCTSTR lpPathName);
+
+    static SetDllDirectory_t s_pfnSetDllDirectory = (SetDllDirectory_t) -1;
+
+    if ( s_pfnSetDllDirectory == (SetDllDirectory_t) -1 )
+    {
+        /*
+        Should wxLoadedDLL ever not be used here (or rather, the
+        wxDL_GET_LOADED flag isn't used), infinite recursion will take
+        place (unless s_pfnSetDllDirectory is set to NULL here right
+        before loading the DLL).
+        */
+        wxLoadedDLL dllKernel("kernel32.dll");
+
+        wxDL_INIT_FUNC_AW(s_pfn, SetDllDirectory, dllKernel);
+    }
+
+    if (s_pfnSetDllDirectory)
+    {
+        s_pfnSetDllDirectory(path.t_str());
+    }
+
     wxDllType handle = ::LoadLibrary(libname.t_str());
 
     // reset the search path
-    ::SetDllDirectory(NULL);
+    if (s_pfnSetDllDirectory)
+    {
+        s_pfnSetDllDirectory(NULL);
+    }
+
     return handle;
 }
 